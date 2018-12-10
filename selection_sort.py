b5516793f46dd8f10d65030996537cfd0986a2e0
Murphy Zheng <murphy@mieasy.com>
2018-12-10 13:56:04 +0800

    Deeper understand

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallestIndex = findSmallest(arr)
+        smallestItem = arr.pop(smallestIndex)
+        newArr.append(smallestItem)
+    return newArr
+
+
 def findSmallest(arr):
     smallest = arr[0]
     smallest_index = 0
@@ -8,13 +17,6 @@ def findSmallest(arr):
             smallest = arr[i]
             smallest_index = i
     return smallest_index
-
-def selectionSort(arr):
-    newArr = []
-    for i in range(len(arr)):
-        smallestIndex = findSmallest(arr)
-        newArr.append(arr.pop(smallestIndex))
-    return newArr
 
 originArr = [7, 5, 9, 3, 2, 6]
 print(selectionSort(originArr))
